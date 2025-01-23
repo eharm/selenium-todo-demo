fe01ab9bed7eb5f97f138281afd82aeb83ee7e98
Evan Harmony <evan_harmony@hotmail.com>
2025-01-23 16:03:58 -0500

    Add project files.

---
+++ Tests/TodoDemo.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TodoDemo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDri
+        }
+    }
+}
---
+++ TodoDemo/TodoDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoDemo
+{
+    internal class TodoDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
